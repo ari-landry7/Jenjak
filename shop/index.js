b025e8fb3fec1945ce4cb2c126d3504de2b23844
Ari <arilandry7@gmail.com>
2025-04-04 00:39:40 -0500

    start of shop

---
+++ shop/index.js
@@ -0,0 +1,11 @@
+const products = [
+    { name: "Stickers", price: 1, id: 1, quantity: 1 },
+    { name: "Holiday Cards", price: 3, id: 2, quantity: 1 },
+    { name: "Coloring Books", price: 5, id: 3, quantity: 1 },
+    { name: "Small Goblin Treasure Jar", price: 20, id: 4, quantity: 1 },
+    { name: "Large Goblin Treasure Jar", price: 40, id: 5, quantity: 1 }
+];
+
+const productsHTML = products.map(
+    (product)
+)
